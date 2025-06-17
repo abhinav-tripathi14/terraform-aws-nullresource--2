870327d8b25ac50f5ccfb2b6dfe88fa4079a68f3
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-06-17 15:50:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ module "nullresource--1" {
 source  = "app.terraform.io/Test-Abhinav/nullresource--1/aws"
 version = "1.0.4"
 }
+resource "null_resource" "test"{
+provisioner "local-exec" {
+    command = "bash ${path.root}/scripts/test.sh"
+  }
+}
