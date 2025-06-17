dd768b46a4f671f52230fb48a2de2ee1f4bebf01
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-06-17 22:40:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ source  = "app.terraform.io/Test-Abhinav/nullresource--1/aws"
 version = "1.0.4"
 #source = "git::ssh://git@github.com/abhinav-tripathi14/terraform-aws-nullresource--1.git?ref=main"
 }
+/*
 resource "null_resource" "test"{
 provisioner "local-exec" {
     command = "./scripts/test.sh"
@@ -19,3 +20,4 @@ provisioner "local-exec" {
     command = "pwd"
   }
 }
+*/
